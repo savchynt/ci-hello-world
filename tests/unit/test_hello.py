9722f10889d0608b20fed07c99517f63a5c85927
Taras Savchyn <savchyntm@gmail.com>
2018-11-28 01:49:47 +0200

    Fix new imports

--- tests/unit/test_hello.py
+++ tests/unit/test_hello.py
@@ -2,23 +2,23 @@ import sys
 import unittest
 
 sys.path.append('.')  # for units tests
-from app.hello import hello
+from src.hello import hello
 
 
 class TestHello(unittest.TestCase):
     def test_type(self):
-        output = hello()
+        output = hello.hello()
         self.assertIsInstance(output, str, 'Wrong type!')
 
     def test_content(self):
         greetings = 'Hello'
         subject = 'World'
-        output = hello()
+        output = hello.hello()
         self.assertIn(greetings, output, 'You should Say Hello!')
         self.assertIn(subject, output, 'Say Hello to the World!')
 
     def test_grammar(self):
-        output = hello()
+        output = hello.hello()
         self.assertIn('!', output, "Use '!' in Greetings!")
 
 
